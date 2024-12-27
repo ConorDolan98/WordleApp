d36c5c7f7eacbf145e6828324f68ca1afc30ec88
ConorDolan98 <g00346153@atu.ie>
2024-12-27 13:40:03 +0000

    Updated so it is linked with the api and reads from it. Uses databinding to update ui

---
+++ WordleGame/Services/WordleService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordleGame.Model;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace WordleGame.Services
+{
+    public class WordleService
+    {
+        private readonly HttpClient httpClient;
+        private List<Wordle> wordsList = new();
+
+        public WordleService()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public async Task<List<Wordle>> GetWords()
+        {
+            //Checks if words are loaded
+            if (wordsList.Count > 0)
+                return wordsList;
+
+            //api
+            var wordUrl = "https://raw.githubusercontent.com/DonH-ITS/jsonfiles/main/words.txt";
+
+            //Retrieving the words from the api
+            var response = await httpClient.GetAsync(wordUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var text = await response.Content.ReadAsStringAsync();
+                var words = text.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+                // Create Wordle objects for each word
+                foreach (var word in words)
+                {
+                    wordsList.Add(new Wordle { Word = word.Trim() });
+                }
+            }
+            else
+            {
+                throw new Exception("Unable to retrieve words from the API.");
+            }
+
+            return wordsList;
+        }
+    }
+}
+
---
+++ WordleGame/ViewModel/WordleViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WordleGame.Model;
+using WordleGame.Services;
+
+namespace WordleGame.ViewModel
+{
+    public class WordleViewModel : BaseViewModel
+    {
+        WordleService wordleService;
+        public ObservableCollection<Wordle> Words { get; } = new();
+
+        public Command GetWordsCommand { get; }
+        public WordleViewModel(WordleService wordleService)
+        {
+            Title = "Wordle";
+            this.wordleService = wordleService;
+            GetWordsCommand = new Command(async () => await GetWordsAsync());
+        }
+
+        async Task GetWordsAsync()
+        {
+            if (IsBusy)
+                return;
+
+            try
+            {
+                IsBusy = true;
+                var words = await wordleService.GetWords();
+
+                if (Words.Count != 0)
+                    Words.Clear();
+
+                foreach (var word in words)
+                {
+                    Words.Add(word);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await Shell.Current.DisplayAlert("Error", "Unable to get words", "OK");
+
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
